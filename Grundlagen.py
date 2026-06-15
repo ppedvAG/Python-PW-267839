b7c4edfb1bb097f33a6067d87dd39578fe4f3ce8
de2 <de2@WIN11-95>
2026-06-15 10:52:42 +0200

    übung modul 2

--- Grundlagen.py
+++ Grundlagen.py
@@ -39,3 +39,27 @@ print(quotient)
 print(x%2 == 0) # False: 5 ist nicht durch 2 teilbar
 
 print(3**2) # hoch rechnen, 2^3
+
+vorname = "Max"
+nachname = "Mustermann"
+
+name = vorname+nachname
+print(name.count("m")+name.count("M"))
+print(name.upper().count("M"))
+
+meinVorname = "dominik"
+print(meinVorname.capitalize())
+
+# variante1
+lenVorname = len(vorname)
+lenNachname = len(nachname)
+lenMeinVorname = len(meinVorname)
+print(lenVorname+lenNachname+lenMeinVorname)
+
+# variante1 verkürzt
+print(len(vorname)+len(nachname)+len(meinVorname))
+
+# variante2
+gesamt = vorname+nachname+meinVorname
+print(len(gesamt))
+
